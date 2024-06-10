9d6e5fc056bf15facb548500620f978f11c67460
DmitriiN <nikitin79060773007@gmail.com>
2024-06-10 08:41:16 +0300

    + func regService

--- dtservice1/main.go
+++ dtservice1/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -55,6 +60,9 @@ func initial() {
 func main() {
 	initial()
 
+	// регистрация сервиса на gateway
+	regService()
+
 	router := mux.NewRouter()
 
 	router.HandleFunc(configuration.HttpDomain+"/employee/set", PostEmployeeSet).Methods("POST")
@@ -75,6 +83,7 @@ func main() {
 	router.HandleFunc(configuration.HttpDomain+"/order/set", PostOrderSet).Methods("POST")
 	router.HandleFunc(configuration.HttpDomain+"/order/list", GetOrderList).Methods("GET")
 	router.HandleFunc(configuration.HttpDomain+"/order/delete", DeleteOrder).Methods("DELETE")
+	router.HandleFunc(configuration.HttpDomain+"/order/state/list", GetOrderStateList).Methods("GET")
 	router.HandleFunc(configuration.HttpDomain+"/role/list", GetRoleList).Methods("GET")
 
 	log.Println("Init router handlers...")
@@ -101,4 +110,47 @@ func main() {
 
 	log.Printf("Starting DepTrans API Service v.%s on port%s", configuration.Version, configuration.HttpPort)
 	log.Fatal(server.ListenAndServe())
+}
+
+func regService() {
+	filename, err := os.Open("init.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer filename.Close()
+
+	data, err := io.ReadAll(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var service_ Service
+	jsonErr := json.Unmarshal(data, &service_)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	log.Println(service_)
+
+	data, err = json.Marshal(service_)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	req, err := http.NewRequest("POST", "localhost:8080/register_service", bytes.NewBuffer(data))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	log.Println(resp.Status)
 }
