05bf5f731031d70fc7b99726b590c6c6534089f1
phobos <uranius666@gmail.com>
2024-06-15 00:06:44 +0300

    начал делать клиент для выборки из db_service

--- rust_services/order_service/src/http/client.rs
+++ rust_services/order_service/src/http/client.rs
@@ -1,6 +1,13 @@
-use hyper::{body::{Incoming, Bytes}, Request, Response, Uri};
+use std::net::SocketAddr;
+
+use http_body_util::{BodyExt, Full};
+use hyper::{body::{Bytes, Incoming}, header::{self, HOST}, Request, Response, StatusCode, Uri};
+use hyper_util::rt::TokioIo;
 use serde::{de::DeserializeOwned, Serialize};
+use tokio::net::TcpStream;
 use crate::error::OrderError;
+pub type BoxBody = http_body_util::combinators::BoxBody<Bytes, hyper::Error>;
+
 
 pub async fn post<I: Serialize, O: DeserializeOwned>(uri: Uri, obj: &I) -> Result<O, OrderError>
 {
@@ -39,4 +46,63 @@ pub async fn post<I: Serialize, O: DeserializeOwned>(uri: Uri, obj: &I) -> Resul
         return  Ok(resp);    
     }
     Ok(send)
+}
+
+
+
+pub fn to_body(bytes: Bytes) -> BoxBody
+{
+    Full::new(bytes)
+        .map_err(|never| match never {})
+    .boxed()
+}  
+pub fn empty_response(code: StatusCode) -> Response<BoxBody>
+{
+    Response::builder()
+    .status(code)
+    .body(to_body(Bytes::new())).unwrap()
+}
+
+pub fn error_response(err: String, code: StatusCode) -> Response<BoxBody>
+{
+    standart_headers()
+    .status(code)
+    .body(to_body(Bytes::from(err))).unwrap()
+}
+pub fn error_empty_response(code: StatusCode) -> Response<BoxBody>
+{
+    standart_headers()
+    .status(code)
+    .header(header::CONTENT_TYPE, "text/html; charset=utf-8")
+    .body(to_body(Bytes::new())).unwrap()
+}
+pub fn ok_response(msg: String) -> Response<BoxBody>
+{
+    standart_headers()
+    .status(StatusCode::OK)
+    //.header(ACCESS_CONTROL_ALLOW_HEADERS, "User-Id")
+    .body(to_body(Bytes::from(msg))).unwrap()
+}
+pub fn json_response<S: Serialize>(obj: &S) -> Response<BoxBody>
+{
+    let result = serde_json::to_string(obj).unwrap();
+    standart_headers()
+    .status(StatusCode::OK)
+    .body(to_body(Bytes::from(result))).unwrap()
+}
+
+pub fn unauthorized_response() -> Response<BoxBody>
+{
+    standart_headers()
+    .status(StatusCode::UNAUTHORIZED)
+    .body(to_body(Bytes::from_static(b"Unauthorized")))
+    .unwrap()
+}
+
+pub fn standart_headers() -> hyper::http::response::Builder
+{
+    Response::builder()
+    //.header(ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:5173")
+    //.header(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true")
+    //.header(ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, OPTIONS")
 }
